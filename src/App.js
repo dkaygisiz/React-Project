761095bc2efa4b6e0f5460fcb88c2bfc7d84584f
dkaygisiz <kemaldogukankaygisiz@gmail.com>
2022-09-17 00:58:05 +0300

    commit 1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-import logo from "./logo.svg";
-import "./App.css";
+import { useState, useEffect } from "react";
+import Card from "./components/Card";
 
 function App() {
+  const [posts, setPosts] = useState([]);
+
+  function getFetchUsers() {
+    fetch("http://localhost:8000/posts")
+      .then((res) => res.json())
+      .then((data) => setPosts(() => data));
+  }
+
+  useEffect(() => getFetchUsers(), []);
+
+  const kartlar = posts.map((post) => (
+    <Card
+      id={post.id}
+      body={post.body}
+      imageId={post.imageId}
+      title={post.title}
+      userId={post.userId}
+    />
+  ));
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <>
+      <div className="container">
+        <div className="row">{kartlar}</div>
+      </div>
+    </>
   );
 }
 
